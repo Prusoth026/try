55833fbd8a221c8060883ca16b75ee3f269c4947
Prusoth <mrprusothaman@gmail.com>
2025-01-30 18:43:14 +0530

    Web Initial

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ['@/main.tsx'],  // Optional externalization
+    },
+  },
+}));
